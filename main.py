92f134ee67fe4caa79b3d8a00763583a9e26b694
Душин Михаил <m-garfield@rambler.ru>
2022-07-27 14:20:05 +0300

    work_version

--- main.py
+++ main.py
@@ -1,28 +1,27 @@
+import json
 import requests
 import yadisk
-from pprint import pprint
-
+json_dic = []
 with open ("file_token", "r") as file_token:
     token = file_token.read().strip()
-
-# token = input("Введите токен = ")
-
-# owner_id = input("Введите ID = ")
+token_ya = input("Введите токен с Yandex полигона: ")
+owner_id = input("Введите ID: ")
+count = 5
+count = input("Введите желаемое количество загружаемых фото: ")
 url = 'https://api.vk.com/method/photos.get'
 params = {
-    "owner_id": "629727012",
+    "owner_id": owner_id,
     'access_token': token,
     'album_id': "profile",
     "photo_sizes": '1',
     'v': "5.131",
     'extended': '1',
-    "count": '100'
+    "count": count
 }
-
 respond = requests.get(url, params=params)
-# pprint(respond.json())
-with open("file_ya_token", "r") as file_ya_token:
-    token_ya = file_ya_token.read()
+
+# with open("file_ya_token", "r") as file_ya_token:
+#     token_ya = file_ya_token.read()
 
 y = yadisk.YaDisk(token=token_ya)
 if y.check_token():
@@ -30,17 +29,15 @@ if y.check_token():
         y.mkdir("/Dir_of_Foto")
         print('Папка "/Dir_of_Foto" создана, загружаем фото')
     else:
-        print("Папка существует, загружаем фото ")
-for foto_big_size in respond.json()["response"]['items']:
-    name_foto = str(foto_big_size["likes"]['count']) + "/" + str(foto_big_size["date"])
+        print("Папка существуеn и готова к загрузке фотографий")
+
+for foto_big_size in respond.json()['response']['items']:
+    name_foto = "/Dir_of_Foto/" + str(foto_big_size["likes"]['count']) + "_" + str(foto_big_size["date"])
     url_foto = foto_big_size["sizes"][-1]['url']
-
-    # print("имя данной фотки будет: ", str(Foto_big_size["likes"]['count']) + "/" + str(Foto_big_size["date"]), ", a ee url:",Foto_big_size["sizes"][-1]['url'])
-
-
-    y.download("/Dir_of_Foto"+ name_foto, url_foto)
-
-# вторая  часть , отправляем  фотку на яндекс диск
+    y.upload_url(url_foto, name_foto)
+    json_dic.append({"file_name": str(foto_big_size["likes"]['count']) + '.jpg', "size": str(foto_big_size["sizes"][-1]['type'])})
+    with open("file_json", "w") as file_json:
+        json.dump(json_dic, file_json)
 
 # токен     AQAAAABQRXTlAAhDhQfNmGSPcUUeiWPRpyPWFhM
 
