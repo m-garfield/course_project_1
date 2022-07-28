a739b510c2d3262a906278634e68f33994803cdf
Душин Михаил <m-garfield@rambler.ru>
2022-07-28 13:54:48 +0300

    Final

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import json
 import requests
 import yadisk
+
+
+import time
+from tqdm import tqdm
+
+
+
 json_dic = []
-with open ("file_token", "r") as file_token:
+with open("file_token", "r") as file_token:
     token = file_token.read().strip()
 token_ya = input("Введите токен с Yandex полигона: ")
 owner_id = input("Введите ID: ")
@@ -19,25 +26,18 @@ params = {
     "count": count
 }
 respond = requests.get(url, params=params)
-
-# with open("file_ya_token", "r") as file_ya_token:
-#     token_ya = file_ya_token.read()
-
 y = yadisk.YaDisk(token=token_ya)
 if y.check_token():
     if not y.is_dir("/Dir_of_Foto"):
         y.mkdir("/Dir_of_Foto")
-        print('Папка "/Dir_of_Foto" создана, загружаем фото')
-    else:
-        print("Папка существуеn и готова к загрузке фотографий")
-
-for foto_big_size in respond.json()['response']['items']:
-    name_foto = "/Dir_of_Foto/" + str(foto_big_size["likes"]['count']) + "_" + str(foto_big_size["date"])
-    url_foto = foto_big_size["sizes"][-1]['url']
-    y.upload_url(url_foto, name_foto)
-    json_dic.append({"file_name": str(foto_big_size["likes"]['count']) + '.jpg', "size": str(foto_big_size["sizes"][-1]['type'])})
-    with open("file_json", "w") as file_json:
-        json.dump(json_dic, file_json)
+for i in tqdm(range(int(count))):
+    for foto_big_size in respond.json()['response']['items']:
+        name_foto = "/Dir_of_Foto/" + str(foto_big_size["likes"]['count']) + "_" + str(foto_big_size["date"])
+        url_foto = foto_big_size["sizes"][-1]['url']
+        y.upload_url(url_foto, name_foto)
+        json_dic.append({"file_name": str(foto_big_size["likes"]['count']) + '.jpg', "size": str(foto_big_size["sizes"][-1]['type'])})
+        with open("file_json", "w") as file_json:
+            json.dump(json_dic, file_json)
 
 # токен     AQAAAABQRXTlAAhDhQfNmGSPcUUeiWPRpyPWFhM
 
